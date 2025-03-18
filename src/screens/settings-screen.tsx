68d8e9a139ab4e5096b7a20c3ade27ecce9bb8b0
Maratib Ali Khan <maratib@gmail.com>
2025-03-18 05:12:19 +0500

    material added

--- src/screens/settings-screen.tsx
+++ src/screens/settings-screen.tsx
@@ -1,12 +1,13 @@
 import { Text, View } from 'react-native'
 import React, { Component } from 'react'
+import { Button } from '@react-native-material/core'
 
 class SettingsScreen extends Component {
   render() {
     return (
        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <Text>Settings Screen</Text>
-            {/* <Icon name="home" size={50} color="blue" /> */}
+            <Button title="Button" onPress={() => console.log("Button Clicked")}/>;
       
           </View>
     )
